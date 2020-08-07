2015a0bede9145a5d37b5753a726cef7be846362
Shawn Benny <sbenn9210@gmail.com>
2020-08-06 19:18:47 -0500

    inital commit. Working on crud resolvers

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+let idCount = links.length;
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (parent, args) => links.filter((item) => (item.id = args.id)),
+  },
+
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      updatedlink = {
+        id: args.id,
+        description: args.description,
+        url: args.url,
+      };
+
+      let oldLink = links.find((item) => item.test === first);
+      let index = links.indexOf(oldLink);
+
+      links.splice(index, 1, updateLink);
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
